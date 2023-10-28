a700568ddf655ca19d0f2300b7e59edcd4eb6897
lon <alfardproject@gmail.com>
2023-10-27 20:02:18 -0700

    x

---
+++ Assets/Scenes/GameLevel/cutLevels/keystorepass.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keystorepass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //54486982lon
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
