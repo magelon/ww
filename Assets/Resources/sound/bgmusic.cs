270fdb19a32de356a8824228a1966e6cddcf146b
lon <alfardproject@gmail.com>
2023-10-27 22:49:41 -0700

    g

---
+++ Assets/Resources/sound/bgmusic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgmusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //https://opengameart.org/content/drums-of-dawn
+    //Copyright/Attribution Notice: 
+    //Tomasz Kucza / magory.games
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/factions/prefabs/Factions/spawnManager.cs
+++ Assets/factions/prefabs/Factions/spawnManager.cs
@@ -85,56 +85,56 @@ public class spawnManager : MonoBehaviour
 
 
         //check level is passed or not and given things
-        if (GameData.getInstance().levelPassed < 4)
-        {
-            GameData.getInstance().levelPassed = PlayerPrefs.GetInt("levelPassed", 0);
-            //Debug.Log("current passed level = " + GameData.getInstance().levelPassed);
-            if (GameData.getInstance().levelPassed == 0)
-            {
-                //unlock item0
-                PlayerPrefs.SetInt("item_0", 1);
-                GameManager.getInstance().updateItemLock();
-                SpawnButtonGroup[0].transform.Find("Image").GetComponent<Image>().sprite =
-                Resources.Load<Sprite>("sumPrefabs/itemImgs/item0");
-                PlayerPrefs.SetString("currentEquip0","item0");
-                // if (intro)
-                // {
-                //     intro.SetActive(true);
-                // }
+        // if (GameData.getInstance().levelPassed < 4)
+        // {
+        //     GameData.getInstance().levelPassed = PlayerPrefs.GetInt("levelPassed", 0);
+        //     //Debug.Log("current passed level = " + GameData.getInstance().levelPassed);
+        //     if (GameData.getInstance().levelPassed == 0)
+        //     {
+        //         //unlock item0
+        //         PlayerPrefs.SetInt("item_0", 1);
+        //         GameManager.getInstance().updateItemLock();
+        //         SpawnButtonGroup[0].transform.Find("Image").GetComponent<Image>().sprite =
+        //         Resources.Load<Sprite>("sumPrefabs/itemImgs/item0");
+        //         PlayerPrefs.SetString("currentEquip0","item0");
+        //         // if (intro)
+        //         // {
+        //         //     intro.SetActive(true);
+        //         // }
 
-            }
-            if (GameData.getInstance().levelPassed == 2)
-            {
-                PlayerPrefs.SetInt("item_1", 1);
-                GameManager.getInstance().updateItemLock();
-                SpawnButtonGroup[0].transform.Find("Image").GetComponent<Image>().sprite =
-                Resources.Load<Sprite>("sumPrefabs/itemImgs/item0");
-                SpawnButtonGroup[1].transform.Find("Image").GetComponent<Image>().sprite =
-                Resources.Load<Sprite>("sumPrefabs/itemImgs/item1");
-                PlayerPrefs.SetString("currentEquip1", "item1");
-                // if (intro)
-                // {
-                //     intro.SetActive(true);
-                // }
+        //     }
+        //     if (GameData.getInstance().levelPassed == 2)
+        //     {
+        //         PlayerPrefs.SetInt("item_1", 1);
+        //         GameManager.getInstance().updateItemLock();
+        //         SpawnButtonGroup[0].transform.Find("Image").GetComponent<Image>().sprite =
+        //         Resources.Load<Sprite>("sumPrefabs/itemImgs/item0");
+        //         SpawnButtonGroup[1].transform.Find("Image").GetComponent<Image>().sprite =
+        //         Resources.Load<Sprite>("sumPrefabs/itemImgs/item1");
+        //         PlayerPrefs.SetString("currentEquip1", "item1");
+        //         // if (intro)
+        //         // {
+        //         //     intro.SetActive(true);
+        //         // }
 
-            }
-            if (GameData.getInstance().levelPassed == 3)
-            {
-                PlayerPrefs.SetInt("item_2", 1);
-                GameManager.getInstance().updateItemLock();
-                SpawnButtonGroup[0].transform.Find("Image").GetComponent<Image>().sprite =
-                Resources.Load<Sprite>("sumPrefabs/itemImgs/item0");
-                SpawnButtonGroup[1].transform.Find("Image").GetComponent<Image>().sprite =
-                Resources.Load<Sprite>("sumPrefabs/itemImgs/item1");
-                SpawnButtonGroup[2].transform.Find("Image").GetComponent<Image>().sprite =
-                Resources.Load<Sprite>("sumPrefabs/itemImgs/item2");
-                PlayerPrefs.SetString("currentEquip2", "item2");
-                // if (intro)
-                // {
-                //     intro.SetActive(true);
-                // }
-            }
-        }
+        //     }
+        //     if (GameData.getInstance().levelPassed == 3)
+        //     {
+        //         PlayerPrefs.SetInt("item_2", 1);
+        //         GameManager.getInstance().updateItemLock();
+        //         SpawnButtonGroup[0].transform.Find("Image").GetComponent<Image>().sprite =
+        //         Resources.Load<Sprite>("sumPrefabs/itemImgs/item0");
+        //         SpawnButtonGroup[1].transform.Find("Image").GetComponent<Image>().sprite =
+        //         Resources.Load<Sprite>("sumPrefabs/itemImgs/item1");
+        //         SpawnButtonGroup[2].transform.Find("Image").GetComponent<Image>().sprite =
+        //         Resources.Load<Sprite>("sumPrefabs/itemImgs/item2");
+        //         PlayerPrefs.SetString("currentEquip2", "item2");
+        //         // if (intro)
+        //         // {
+        //         //     intro.SetActive(true);
+        //         // }
+        //     }
+        // }
 
     }
 
