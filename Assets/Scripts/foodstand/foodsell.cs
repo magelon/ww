18bde7db0ea313153bb01b185333cd1e8cff4e2b
lon <alfardproject@gmail.com>
2024-09-21 23:54:54 -0700

    u

--- Assets/Scripts/foodstand/foodsell.cs
+++ Assets/Scripts/foodstand/foodsell.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class foodsell : MonoBehaviour
 {
+    public foodstandsystem fs;
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Print the name of the object that enters the trigger
         Debug.Log("Object Entered Trigger: " + other.gameObject.name);
+        if(fs){
+            fs.itemsold();
+        }
     }
 
 }
--- Assets/Scripts/foodstand/foodstandsystem.cs
+++ Assets/Scripts/foodstand/foodstandsystem.cs
@@ -1,40 +1,138 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using UnityEngine.UI;
+
+[System.Serializable]
+public class FooddetailData
+{
+    public int price;
+    public int rate;
+    public string art;
+}
+
+[System.Serializable]
+public class FoodData
+{
+    public string foodName;
+    public FooddetailData foodsdetail;
+}
+
+[System.Serializable]
+public class FoodsData
+{
+    public List<FoodData> foods;
+}
 
 public class foodstandsystem : MonoBehaviour
 {
-    //public List<Recipe> recipes;  // List of possible recipes
+    public TextAsset jsonDataFile;
     public List<GameObject> go;
+    public FoodsData foodsData;
 
-    private string[] stringArray = new string[5]; // Array to hold two strings
-    private int currentIndex = 0; // Tracks which index to add a new string to
+    private List<string> stringList = new List<string>(); 
+
+     void Start()
+    { 
+        if (jsonDataFile != null)
+        {
+            foodsData = JsonUtility.FromJson<FoodsData>(jsonDataFile.text);
+           
+        }
+    }
+
+    //void Updated(){PrintAllElements();}
 
     // Method to add a new string to the array
     public void AddString(string newString)
     {
-        // Add the new string to the current index
-        stringArray[currentIndex] = newString;
-        currentIndex++;
-
-        // If the index exceeds 1 (0 and 1 are the valid indices), reset it to 0
-        if (currentIndex > 4)
+        if (stringList.Count >= 5) // Limit to 5 elements
         {
-            currentIndex = 0;
+            stringList.RemoveAt(0); // Remove the oldest element
         }
+        stringList.Add(newString); // Add the new string
 
-        Debug.Log("Updated array: [" + stringArray[0] + ", " + stringArray[1] + "]");
+        Debug.Log("Updated list: [" + string.Join(", ", stringList) + "]");
     }
 
     public void PrintAllElements()
     {
-        //Debug.Log("Array elements:");
-        for (int i = 0; i < stringArray.Length; i++)
+        for (int i = 0; i < 5; i++)
+    {
+    if (i < go.Count) // Ensure we do not exceed the number of GameObjects
+    {
+        Image im = go[i].GetComponent<Image>();
+
+        // Clear the sprite first
+        im.sprite = null;
+
+        if (i < stringList.Count) // Check if there's a corresponding string in stringList
         {
-            Image im=go[i].GetComponent<Image>();
-            im.sprite = Resources.Load<Sprite>("sumPrefabs/goodImgs/" + stringArray[i]);
-            //Debug.Log("Element " + i + ": " + stringArray[i]);
+            // Attempt to load the sprite
+            Sprite loadedSprite = Resources.Load<Sprite>("sumPrefabs/goodImgs/" + stringList[i]);
+
+            if (loadedSprite != null)
+            {
+                im.sprite = loadedSprite; // Set the sprite if found
+            }
+            else
+            {
+                Debug.LogWarning("Sprite not found for: " + stringList[i]);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No string available for slot " + i);
+        }
+    }
+    }
+
+    }
+
+    public void itemsold()
+    {
+        List<int> indicesToRemove = new List<int>(); 
+        for (int i = 0; i < go.Count && i < stringList.Count; i++)
+        {
+            Image imageComponent = go[i].GetComponent<Image>();
+
+            if (imageComponent != null && imageComponent.sprite != null)
+            {
+                // Get the name of the sprite currently in the Image component
+                string imageNamee = imageComponent.sprite.name;
+
+                Debug.Log("Image name: " + imageNamee);
+
+                // Find the corresponding FoodData by matching the imageName
+                FoodData soldFood = foodsData.foods.Find(food => food.foodName == imageNamee);
+
+                if (soldFood != null)
+                {
+                    Debug.Log("Food sold: " + soldFood.foodName + ", Price: " + soldFood.foodsdetail.price);
+                    imageComponent.sprite=null;
+                    indicesToRemove.Add(i);
+                }
+                else
+                {
+                    Debug.LogError("Food not found for image: " + imageNamee);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("No sprite found for GameObject at index: " + i);
+            }
+            for (int j = indicesToRemove.Count - 1; j >= 0; j--)
+            {
+                int index = indicesToRemove[j];
+                if (index < stringList.Count) // Ensure index is within range
+            {
+                stringList.RemoveAt(index);
+            }
+            }
+            stringList.Sort();
+            PrintAllElements();
+            Debug.Log("Sorted stringList: [" + string.Join(", ", stringList) + "]");
         }
     }
 
