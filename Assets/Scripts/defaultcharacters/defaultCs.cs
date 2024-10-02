ecc48194b55fb4504a44f3715bd3db2939d80c19
lon <alfardproject@gmail.com>
2024-10-01 22:26:23 -0700

    d

---
+++ Assets/Scripts/defaultcharacters/defaultCs.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defaultCs : MonoBehaviour
+{
+    List<string> stringList = new List<string> {"sabar", "lina", "keiqin"};
+    public  static List<string> equiptedL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        equiptedL = new List<string>();
+        //PlayerPrefs.DeleteAll();
+        //Loop through the collection
+         
+        foreach (string fruit in stringList)
+        {
+            if (!PlayerPrefs.HasKey(fruit))
+            {
+                equiptedL.Add(fruit);
+                PlayerPrefs.SetInt(fruit, 1);
+                PlayerPrefs.SetInt(fruit+"Level",1);
+                GameManager.getInstance().init();
+            }
+        }
+
+        for (int i = 0; i < equiptedL.Count; i++)
+        {
+        PlayerPrefs.SetString("currentEquip" + i, equiptedL[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
