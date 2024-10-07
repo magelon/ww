9e362f00cae5558dd480ddfa28adf72e1aa0a33b
lon <alfardproject@gmail.com>
2024-10-06 19:52:21 -0700

    f

--- Assets/Scripts/ttbScript/StartSplash.cs
+++ Assets/Scripts/ttbScript/StartSplash.cs
@@ -1,23 +1,23 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System;
 
 
 public class StartSplash : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-                date = DateTime.Now.ToString().Substring(0, 9);
+                //PlayerPrefs.DeleteAll();
+                string date = DateTime.Now.ToString().Substring(0, 9);
                 if (PlayerPrefs.GetInt(date) == 1)
                 {
                         SceneManager.LoadScene("MainMenu");
                 }else{
                         SceneManager.LoadScene("DailyBonus");
                 }
-        //		PlayerPrefs.DeleteAll();
         //		MadLevelProfile.Reset ();
         //		bool istrial = UnityPluginForWindowsPhone.Class1.SwitchTrialMode();//comment this when on real version.
-        
 	}
 	
 
